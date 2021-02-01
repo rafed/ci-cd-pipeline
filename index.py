ed0e12439efab9e08d92424380e7707684264652
Rafed Muhammad Yasir <rafed123@yahoo.com>
2021-02-01 21:03:37 +0600

    Fix add function

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return {
         'msg': 'Hello World!',
-        'app_version': '3'
+        'app_version': '4'
     }
 
 @app.route('/add/<a>/<b>')
@@ -17,6 +17,12 @@ def add_route(a, b):
 
 # Input can be both strings and digits
 def add(a,b):
+    if type(a) == str and not a.isdigit():
+        return "Invalid Input"
+
+    if type(b) == str and not b.isdigit():
+        return "Invalid Input"
+
     a = int(a)
     b = int(b)
     return a + b
@@ -32,8 +38,3 @@ if __name__ == "__main__":
 
 
 
-# if type(a) == str and not a.isdigit():
-#     return "Invalid Input"
-
-# if type(b) == str and not b.isdigit():
-#     return "Invalid Input"
